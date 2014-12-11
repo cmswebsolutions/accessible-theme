a885f7bc52dc7766c8d4254ab6409b0a747a273f
Mark <minimac@Mini-Mac.local>
2014-12-11 17:00:57 -0500

    Updated Lightbox

---
+++ contact.php
@@ -0,0 +1,15 @@
+ <?php /* Template Name: Contact Page */ ?>
+
+<?php get_header(); ?>
+	<!-- <?php echo basename( __FILE__ ); ?> -->
+
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+		<div id="content" role="main">
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</div>
+
+	<?php endwhile; ?>
+
+<?php get_footer(); ?>
