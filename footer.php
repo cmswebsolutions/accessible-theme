070ba6db5c76cb02f161e380ddcb1facd52c480f
Mark <minimac@Mini-Mac.local>
2014-12-15 14:22:53 -0500

    Turned the lightbox into a plugin

--- footer.php
+++ footer.php
@@ -15,25 +15,6 @@
 			</div>
 		</footer>
 
-
-
-
-
-		<div id="lightbox" tabindex="0">
-			<div class="image">
-				<div id="img-box">
-					<img id="photo" src="" alt=""/>
-					<div class="caption" aria-live="polite" aria-relevant="additions" ></div>
-				</div>
-
-				<button id="prev"><span class="hidden">Previous</span><img src="<?php echo bloginfo('template_directory'); ?>/img/prev.svg" alt="" /></button>
-				<button id="next"><span class="hidden">Next</span><img src="<?php echo bloginfo('template_directory'); ?>/img/next.svg" alt="" /></button>
-				<button id="close">Close<span class="hidden"> image slideshow</span></button>
-				<span class="loading">Loading...</span>
-				<span class="height"></span>
-			</div>
-		</div>
-
 	<?php wp_footer(); ?>
 	</body>
 </html>
